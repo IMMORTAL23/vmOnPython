5365bffdf1ded899e12149815a04bb5894cde6e0
WinSrc <96529985+WinSrc@users.noreply.github.com>
2022-02-12 21:50:08 +0700

    Add files via upload

--- memory/works.py
+++ memory/works.py
@@ -1,3 +1,4 @@
+import requests  # импортируем модул
 def edit_file(filename,string):
     wr = open(filename,"w")
     wr.write(string)
@@ -7,3 +8,9 @@ def read(filename):
     readlines = wr.read()
     wr.close()
     return readlines
+def download_file(http_url,file_path):
+
+    f = open(file_path, "wb")  # открываем файл для записи, в режиме wb
+    ufr = requests.get(http_url)  # делаем запрос
+    f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
+    f.close()
