4c850078b34b6ef66e65924842244dea5e29eb90
WinSrc <96529985+WinSrc@users.noreply.github.com>
2022-02-12 21:49:29 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib import byte_analizer as ba
 
 fr = open("bcode.lbc","r")
 Splitted = fr.read().split("\n")
-
+sh = 1
 for i in Splitted:
-    ba.analize(i)
+    sh += 1
+    ba.analize(string=i,str=sh)
